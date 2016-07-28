40b149d05dc04d305ac1ef50b940a4b61e98871b
Chutanyuan <17839302862@163.com>
2016-07-28 12:24:29 +0800

    测试
    测试

--- view传值/testView.m
+++ view传值/testView.m
@@ -14,7 +14,9 @@
     if (self = [super initWithFrame:frame]) {
         
         self.backgroundColor = [UIColor whiteColor];
-        
+        NSArray * array = @[@"10",@"20",@"30",@"50",@"100",@"200",@"300",@"500",@"1000"];
+        NSArray * littleArray = @[@"0.01",@"19.96",@"29.94",@"49.90",@"99.80",@"198.00",@"299.40",@"499.00",@"998.00"];
+
     }
     return self;
 }
